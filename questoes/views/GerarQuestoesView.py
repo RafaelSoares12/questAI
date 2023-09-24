52e368c8f7cbd29a8e73ee540d7d302ea4f65fa3
RafaelSoares12 <soares.lopes@mail.uft.edu.br>
2023-09-24 02:52:28 -0300

    add view que gera as questões

--- questoes/views/GerarQuestoesView.py
+++ questoes/views/GerarQuestoesView.py
@@ -1,5 +1,9 @@
+import os
+from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render
+import requests
+
 
 class GerarQuestoesView(View):
     template_name = 'index.html'
@@ -12,4 +16,25 @@ class GerarQuestoesView(View):
         qtdQuestoes = request.POST.get('quantidade-questoes')
         nvlDificuldade = request.POST.get('nivel-dificuldade')
 
-        print(assunto, qtdQuestoes, nvlDificuldade)
+        mensagens = [
+            {"role": "system", "content": "Você é um assistente para professores que gera questões apenas sobre assuntos escolares, retorne apenas as questões."},
+            {"role": "user", "content": f"Gere {qtdQuestoes} questões sobre {assunto} com nível de dificuldade {nvlDificuldade}."}
+        ]
+
+        url = "https://chatgpt-api8.p.rapidapi.com/"
+
+        headers = {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": os.getenv('RAPID_API_KEY'),
+            "X-RapidAPI-Host": "chatgpt-api8.p.rapidapi.com"
+        }
+
+        response = requests.post(url, json=mensagens, headers=headers)
+
+        if response.status_code == 200:
+            resposta_gerada = response.json()
+            print(resposta_gerada)
+            return JsonResponse({'resposta': resposta_gerada})
+        else:
+            erro_msg = f'Erro na solicitação à API do GPT-3 (status {response.status_code})'
+            return JsonResponse({'erro': erro_msg}, status=response.status_code)

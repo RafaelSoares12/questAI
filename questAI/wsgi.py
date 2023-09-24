476930032ac9b8b30809950642fc4ff3c951686c
RafaelSoares12 <soares.lopes@mail.uft.edu.br>
2023-09-23 23:29:44 -0300

    add variáveis de ambiente

--- questAI/wsgi.py
+++ questAI/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "QuestAI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questAI.settings")
 
 application = get_wsgi_application()

c59cd5270c2fd57f6ff21a1b84d152c4810e04fc
RafaelSoares12 <soares.lopes@mail.uft.edu.br>
2023-09-24 01:22:18 -0300

    add template e add Tailwind

--- questoes/urls.py
+++ questoes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from questoes.views import *
+from questoes.views import GerarQuestoesView
 
 urlpatterns = [
     path("", GerarQuestoesView.as_view(), name="gerar-questoes" ),

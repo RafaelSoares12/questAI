82acffcf0f1710ad27ba48b2e9dfaf68d3fdb697
RafaelSoares12 <soares.lopes@mail.uft.edu.br>
2023-09-24 01:12:56 -0300

    importando urls do app

--- questAI/urls.py
+++ questAI/urls.py
@@ -1,6 +1,16 @@
+from django.conf.urls.static import static
+from django.conf import settings
+
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+
+import questoes.urls
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include(questoes.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
